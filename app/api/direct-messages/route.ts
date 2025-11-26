ebbbc789457560cf63e50e9571d86b04f65d2728
alish magar <alishmagar705@gmail.com>
2025-11-26 21:37:33 +0545

    resolve error

--- app/api/direct-messages/route.ts
+++ app/api/direct-messages/route.ts
@@ -1,11 +1,11 @@
 import { current_profile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { DirectMessage, Message } from "@/lib/generated/prisma/client";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 const MESSAGE_BATCH = 10
 
-export async function GET(req: NextResponse){
+export async function GET(req: NextRequest){
     try {
         const profile = await current_profile()
         const {searchParams} = new URL(req.url)
